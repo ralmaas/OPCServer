f34698b05b5411c125df3ee224954d3ce6d53389
Roar Almaas <roar.almas@gmail.com>
2023-03-24 11:28:33 +0100

    More update

--- OPCServer.py
+++ OPCServer.py
@@ -10,28 +10,6 @@ import json
 # Need to make structure
 from dataclasses import dataclass
 from pprint import pprint
-# 2023-03-23/ralm
-# V1.8
-# Fixed error if meterID not in meter-table
-#
-# V1.7
-# Rewritten some of the "flow"-logic
-#
-# 2023-03-21/ralm
-# V1.6
-# New names from ElWiz
-#
-# 2022-01-07/ralm
-# V1.5:
-# Added voltageL1, voltageL2 and voltageL3
-#
-# 2022-01-06/ralm
-# V1.4:
-# Multiplyfactor implemented
-#
-# 2021-10-26/ralm
-# V1.3:
-#	Reading meterID using as array
 
 # ========================================
 # Configuration section
